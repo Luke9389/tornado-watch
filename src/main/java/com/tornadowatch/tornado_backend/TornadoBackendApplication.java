53f657e9e80541a4ef6eaca142c2d51ba66ba44c
Luke Donahue <luke9389@gmail.com>
2025-07-30 13:43:42 -0700

    Initial Commit - Spring Boot scaffold

---
+++ src/main/java/com/tornadowatch/tornado_backend/TornadoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tornadowatch.tornado_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornadoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tornadowatch/tornado_backend/TornadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tornadowatch.tornado_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
